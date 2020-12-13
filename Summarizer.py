a9777c4bab066770de834af3405028de24050d8e
DashankaNadeeshanDeSilva <dashankanadeeshandesilva@gmail.com>
2020-12-13 23:35:57 +0100

    Add main script and data

---
+++ Summarizer.py
@@ -0,0 +1,232 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import time
+import re
+import pickle
+
+# Load data
+news = pd.read_excel("data/news.xlsx")
+news.drop(['Source ', 'Time ', 'Publish Date'], axis=1, inplace=True)
+# View raw data
+print(news.head())
+
+# Define docs and summaries
+document = news['news']
+summary = news['Headline']
+
+# Data preprocessing
+# for decoder sequence
+summary = summary.apply(lambda x: '<go> ' + x + ' <stop>')
+
+# Tokenization the text into integer tokens
+# since < and > from default tokens cannot be removed
+filters = '!"#$%&()*+,-./:;=?@[\\]^_`{|}~\t\n'
+oov_token = '<unk>'
+
+document_tokenizer = tf.keras.preprocessing.text.Tokenizer(oov_token=oov_token)
+summary_tokenizer = tf.keras.preprocessing.text.Tokenizer(filters=filters, oov_token=oov_token)
+
+document_tokenizer.fit_on_texts(document)
+summary_tokenizer.fit_on_texts(summary)
+
+inputs = document_tokenizer.texts_to_sequences(document)
+targets = summary_tokenizer.texts_to_sequences(summary)
+
+encoder_vocab_size = len(document_tokenizer.word_index) + 1
+decoder_vocab_size = len(summary_tokenizer.word_index) + 1
+
+# vocab_size
+print(encoder_vocab_size, decoder_vocab_size)
+
+# Getting insights on lengths for defining maximum length
+document_lengths = pd.Series([len(x) for x in document])
+summary_lengths = pd.Series([len(x) for x in summary])
+
+# Checking lengths and information
+print(document_lengths.describe())
+print(summary_lengths.describe())
+
+# maxlen
+# taking values > and round figured to 75th percentile at the same time not leaving high variance
+encoder_maxlen = 400
+decoder_maxlen = 75
+
+# Padding/Truncating sequences for identical sequence lengths
+inputs = tf.keras.preprocessing.sequence.pad_sequences(inputs, maxlen=encoder_maxlen, padding='post', truncating='post')
+targets = tf.keras.preprocessing.sequence.pad_sequences(targets, maxlen=decoder_maxlen, padding='post',
+                                                        truncating='post')
+
+# Cerating data pipeline
+inputs = tf.cast(inputs, dtype=tf.int32)
+targets = tf.cast(targets, dtype=tf.int32)
+
+BUFFER_SIZE = 20000
+BATCH_SIZE = 64
+
+dataset = tf.data.Dataset.from_tensor_slices((inputs, targets)).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+
+
+# Positional Encoding for adding notion of position among words
+# Angles for encoding
+def get_angles(position, i, d_model):
+    angle_rates = 1 / np.power(10000, (2 * (i // 2)) / np.float32(d_model))
+    return position * angle_rates
+
+
+# Encoding
+def positional_encoding(position, d_model):
+    angle_rads = get_angles(
+        np.arange(position)[:, np.newaxis],
+        np.arange(d_model)[np.newaxis, :],
+        d_model
+    )
+
+    # apply sin to even indices in the array; 2i
+    angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
+
+    # apply cos to odd indices in the array; 2i+1
+    angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
+
+    pos_encoding = angle_rads[np.newaxis, ...]
+
+    return tf.cast(pos_encoding, dtype=tf.float32)
+
+
+# Masking- padding and look ahead masking
+def create_padding_mask(seq):
+    seq = tf.cast(tf.math.equal(seq, 0), tf.float32)
+    return seq[:, tf.newaxis, tf.newaxis, :]
+
+
+def create_look_ahead_mask(size):
+    mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
+    return mask
+
+
+# Developing the model
+# Scaled dot product
+def scaled_dot_product_attention(q, k, v, mask):
+    matmul_qk = tf.matmul(q, k, transpose_b=True)
+
+    dk = tf.cast(tf.shape(k)[-1], tf.float32)
+    scaled_attention_logits = matmul_qk / tf.math.sqrt(dk)
+
+    if mask is not None:
+        scaled_attention_logits += (mask * -1e9)
+
+    attention_weights = tf.nn.softmax(scaled_attention_logits, axis=-1)
+
+    output = tf.matmul(attention_weights, v)
+    return output, attention_weights
+
+
+# Multi-head attention class
+class MultiHeadAttention(tf.keras.layers.Layer):
+    def __init__(self, d_model, num_heads):
+        super(MultiHeadAttention, self).__init__()
+        self.num_heads = num_heads
+        self.d_model = d_model
+
+        assert d_model % self.num_heads == 0
+
+        self.depth = d_model // self.num_heads
+
+        self.wq = tf.keras.layers.Dense(d_model)
+        self.wk = tf.keras.layers.Dense(d_model)
+        self.wv = tf.keras.layers.Dense(d_model)
+
+        self.dense = tf.keras.layers.Dense(d_model)
+
+    def split_heads(self, x, batch_size):
+        x = tf.reshape(x, (batch_size, -1, self.num_heads, self.depth))
+        return tf.transpose(x, perm=[0, 2, 1, 3])
+
+    def call(self, v, k, q, mask):
+        batch_size = tf.shape(q)[0]
+
+        q = self.wq(q)
+        k = self.wk(k)
+        v = self.wv(v)
+
+        q = self.split_heads(q, batch_size)
+        k = self.split_heads(k, batch_size)
+        v = self.split_heads(v, batch_size)
+
+        scaled_attention, attention_weights = scaled_dot_product_attention(
+            q, k, v, mask)
+
+        scaled_attention = tf.transpose(scaled_attention, perm=[0, 2, 1, 3])
+
+        concat_attention = tf.reshape(scaled_attention, (batch_size, -1, self.d_model))
+        output = self.dense(concat_attention)
+
+        return output, attention_weights
+
+
+# Feed-forward network
+def point_wise_feed_forward_network(d_model, dff):
+    return tf.keras.Sequential([
+        tf.keras.layers.Dense(dff, activation='relu'),
+        tf.keras.layers.Dense(d_model)
+    ])
+
+
+# Transformer encoder class(basic unit)
+class EncoderLayer(tf.keras.layers.Layer):
+    def __init__(self, d_model, num_heads, dff, rate=0.1):
+        super(EncoderLayer, self).__init__()
+
+        self.mha = MultiHeadAttention(d_model, num_heads)
+        self.ffn = point_wise_feed_forward_network(d_model, dff)
+
+        self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+
+        self.dropout1 = tf.keras.layers.Dropout(rate)
+        self.dropout2 = tf.keras.layers.Dropout(rate)
+
+    def call(self, x, training, mask):
+        attn_output, _ = self.mha(x, x, x, mask)
+        attn_output = self.dropout1(attn_output, training=training)
+        out1 = self.layernorm1(x + attn_output)
+
+        ffn_output = self.ffn(out1)
+        ffn_output = self.dropout2(ffn_output, training=training)
+        out2 = self.layernorm2(out1 + ffn_output)
+
+        return out2
+
+
+# Transformer encoder class(basic unit)
+class DecoderLayer(tf.keras.layers.Layer):
+    def __init__(self, d_model, num_heads, dff, rate=0.1):
+        super(DecoderLayer, self).__init__()
+
+        self.mha1 = MultiHeadAttention(d_model, num_heads)
+        self.mha2 = MultiHeadAttention(d_model, num_heads)
+
+        self.ffn = point_wise_feed_forward_network(d_model, dff)
+
+        self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm3 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+
+        self.dropout1 = tf.keras.layers.Dropout(rate)
+        self.dropout2 = tf.keras.layers.Dropout(rate)
+        self.dropout3 = tf.keras.layers.Dropout(rate)
+
+    def call(self, x, enc_output, training, look_ahead_mask, padding_mask):
+        attn1, attn_weights_block1 = self.mha1(x, x, x, look_ahead_mask)
+        attn1 = self.dropout1(attn1, training=training)
+        out1 = self.layernorm1(attn1 + x)
+
+        attn2, attn_weights_block2 = self.mha2(enc_output, enc_output, out1, padding_mask)
+        attn2 = self.dropout2(attn2, training=training)
+        out2 = self.layernorm2(attn2 + out1)
+
+        ffn_output = self.ffn(out2)
+        ffn_output = self.dropout3(ffn_output, training=training)
+        out3 = self.layernorm3(ffn_output + out2)
+
+        return out3, attn_weights_block1, attn_weights_block2
